65738c334582113f789730f4cc2074a15a472581
Joseph Senders <jasenders@gmail.com>
2018-08-12 21:52:38 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,5 +18,9 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     binding.pry
   end
-
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
+  end
 end
